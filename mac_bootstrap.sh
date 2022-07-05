7a60396ece70c4d4446c684b70d1a5d360c7793a
Nicholas Sunday <sunday.nick+gitlab@gmail.com>
2022-07-05 13:04:01 -0500

    Add more logging and check fix config files

--- mac_bootstrap.sh
+++ mac_bootstrap.sh
@@ -2,34 +2,37 @@
 # Git should already be installed
 
 # Install Kitty
+echo "Installing kitty terminal"
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # Copy Kitty Theme
 if [[ ! -d "$HOME/.config/kitty" ]]; then
+	echo "Creating $HOME/.config/kitty directory"
 	mkdir -p "$HOME/.config/kitty"
 fi
 
+echo "Copying Kitty theme config"
 cp current-theme.conf "$HOME/.config/kitty/current-theme.conf"
 
 # Install Homebrew
+echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Brewfile
+echo "Installing Brewfile"
 brew bundle
 
 # Install oh-my-zsh
+echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install vim-plug for neovim
+echo "Installing vim-plug for NeoVim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Setup Plugin Dir
-if [[ ! -d "$HOME/.config/nvim" ]]; then
-	mkdir -p "$HOME/.config/nvim"
-fi
-
 # Install Powerline Fonts
+echo "Installing Powerline Fonts"
 ## clone
 git clone https://github.com/powerline/fonts.git --depth=1
 ## install
@@ -39,8 +42,15 @@ cd fonts || return
 cd .. || return
 rm -rf fonts
 
+# Setup Plugin Dir
+if [[ ! -d "$HOME/.config/nvim" ]]; then
+	echo "Creating $HOME/.config/nvim directory"
+	mkdir -p "$HOME/.config/nvim"
+fi
+
 # Setup Plugins
-cat << EOF > "$HOME/.config/nvim"
+echo "Creating init.vim file with vim plugins"
+cat << EOF > "$HOME/.config/nvim/init.vim"
 " Plugins will be downloaded under the specified directory.
 call plug#begin(has('nvim') ? stdpath('data') . '/plugged' : '~/.vim/plugged')
 
@@ -67,7 +77,14 @@ let g:deoplete#enable_at_startup = 1
 call plug#end()
 EOF
 
+# Setup Vim Dir
+if [[ ! -d "$HOME/.vim/" ]]; then
+	echo "Creating $HOME/.vim"
+	mkdir -p "$HOME/.vim"
+fi
+
 # Setup vimrc
+echo "Creating vimrc file"
 cat << EOF > "$HOME/.vim/vimrc"
 " Linting settings
 let g:ale_lint_on_enter = 0
